73792495a9f8aec28af11494934c197862beb1bc
Shekhar Mishra <shekhar157@gmail.com>
2024-01-28 11:39:51 -0600

    gg

--- main.tf
+++ main.tf
@@ -6,7 +6,3 @@ resource "azurerm_resource_group" "usr1rg" {
   name     = "gitusr1rg"
   location = "West Europe"
 }
-resource "azurerm_resource_group" "usr2rg" {
-  name     = "gitusr2rg"
-  location = "West Europe"
-}
